1c8ab1fd8acaa1aca25198ce7ea60936e75184b3
Mitchell Petellin <petemi09@Mitchells-MacBook-Pro-3.local>
2019-05-29 21:27:11 -0500

    some C++ basics

---
+++ C++/userInput2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string string1;
+    cout << "enter a string: ";
+    cin >> string1;
+
+    cout << "you entered: " << string1 << " " << string1 <<endl;
+
+    return 0;
+}
