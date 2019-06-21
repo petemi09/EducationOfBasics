2e507ef7da6573771e1cddad1002637068dc1e84
Mitchell Petellin <petemi09@Mitchells-MacBook-Pro-3.local>
2019-06-20 22:02:32 -0600

    learned C# inputs

---
+++ DotNet/inputs/simple input/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LearningBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string userInput;
+            Console.Write("Enter something: ");
+            userInput = Console.ReadLine();
+            Console.WriteLine($"You said '{userInput}'");
+        }
+    }
+}
